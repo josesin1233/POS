5b74caa0486b3292a8e0b74dbab5043af9c7f914
Jose <josesito291000@gmail.com>
2025-08-23 02:19:35 -0600

    Add WSGI_APPLICATION for Railway deployment

--- dulceria_pos/settings.py
+++ dulceria_pos/settings.py
@@ -271,3 +271,4 @@ if not DEBUG:
     STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WSGI_APPLICATION = 'dulceria_pos.wsgi.application'
